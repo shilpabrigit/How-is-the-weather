55903d5b9d701ba36f19199fe23651f4f077346c
shilpabrigit <143263155+shilpabrigit@users.noreply.github.com>
2024-03-24 15:16:39 +0530

    Update code.py

--- code.py
+++ code.py
@@ -4,13 +4,8 @@ st.markdown('<h1 style="color:black;">VGG 16 Image classification model</h1>', u
 st.markdown('<h2 style="color:gray;">The image classification model classifies image into following categories:</h2>', unsafe_allow_html=True)
 st.markdown('<h3 style="color:gray;"> Cloudy, Rainy, Sunny, Sunrise_Sunset</h3>', unsafe_allow_html=True)
 
-
-import tensorflow as tf
-from tensorflow import keras
-import streamlit as st
 import numpy as np
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.models import load_model
+
 
 # Load the saved model
 saved_model = load_model("path_to_saved_model.h5")
