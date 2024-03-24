4a1d37762b8a0614b377606d351386b5bb648186
shilpabrigit <143263155+shilpabrigit@users.noreply.github.com>
2024-03-24 15:47:18 +0530

    Update code.py

--- code.py
+++ code.py
@@ -6,3 +6,6 @@ st.write("The following model classified the uploaded images into Cloudy, Rainy,
 import tensorflow
 import keras
 import numpy
+
+#upload file
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
