0059b0fa1468d86e2cab9f70f2efab932394884b
shilpabrigit <143263155+shilpabrigit@users.noreply.github.com>
2024-03-24 15:44:36 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,8 +1,6 @@
 
 import streamlit as st
 
-st.markdown('<h1 style="color:black;">VGG 16 Image classification model</h1>', unsafe_allow_html=True)
-st.markdown('<h2 style="color:gray;">The image classification model classifies image into following categories:</h2>', unsafe_allow_html=True)
-st.markdown('<h3 style="color:gray;"> Cloudy, Rainy, Sunny, Sunrise_Sunset</h3>', unsafe_allow_html=True)
-
+st.title("Weather Recognition model using VGG16")
+st.header("The following model classified the uploaded images into Cloudy, Rainy,Sunny, Sunrise/Sunset")
 
