fdcd22ec85d3d3c45528c631c90b4f4ade70417b
shilpabrigit <143263155+shilpabrigit@users.noreply.github.com>
2024-03-24 15:49:52 +0530

    Update code.py

--- code.py
+++ code.py
@@ -12,4 +12,4 @@ uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 # Display the uploaded image 
 if uploaded_file is not None:
-   st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
+   st.image(uploaded_file, caption="Uploaded Image", use_column_width=False)
