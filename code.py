b2d9f61f50820b975b755932a54c971dbe82cf0c
shilpabrigit <143263155+shilpabrigit@users.noreply.github.com>
2024-03-24 16:06:48 +0530

    Update code.py

--- code.py
+++ code.py
@@ -13,3 +13,7 @@ uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 # Display the uploaded image 
 if uploaded_file is not None:
    st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
+
+# Load the saved model
+        saved_model_path = r"weather_classif/saved_model.pb"
+        saved_model = load_model(saved_model_path)
